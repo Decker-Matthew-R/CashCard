bd43004809ae1802bba39c923f8a5fb094dd3a20
ramc12School <mdeckerr@gmail.com>
2023-05-10 21:59:10 -0500

    05102023

--- src/main/java/com/example/cashcard/CashCard.java
+++ src/main/java/com/example/cashcard/CashCard.java
@@ -1,7 +1,8 @@
 package com.example.cashcard;
 
-public record CashCard(Long id, Double amount) {
 
+// Add this import
+import org.springframework.data.annotation.Id;
 
-
+public record CashCard(@Id Long id, Double amount) {
 }
